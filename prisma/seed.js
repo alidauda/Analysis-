1373d57ecb6caf87ec96ebc17abd53bf18ed1b9b
alidauda <alidauda14@gmail.com>
2024-08-30 22:51:36 +0100

    Refactor data seeding to use batch processing

--- prisma/seed.js
+++ prisma/seed.js
@@ -14,19 +14,38 @@ async function seedDatabase() {
         .on("end", () => resolve(data))
         .on("error", (error) => reject(error));
     });
-
-    await prisma.data.createMany({
-      data: results.map((item) => {
-        const trimmedItem = {
-          ...item,
-          INSTITUTION: item.INSTITUTION.trim().replace(/\s+/g, " "),
-          InstitutionType: item.InstitutionType.trim()
-            .replace(/\s+/g, " ")
-            .toLowerCase(),
-        };
-        return trimmedItem;
-      }),
-    });
+    const len = results.length;
+    const batch = 500;
+    const numberOfBatches = Math.ceil(len / batch);
+    for (let i = 0; i < numberOfBatches; i++) {
+      const start = i * batch;
+      const end = Math.min(len, (i + 1) * batch);
+      const data = results.slice(start, end);
+      await prisma.data.createMany({
+        data: data.map((item) => {
+          const trimmedItem = {
+            ...item,
+            INSTITUTION: item.INSTITUTION.trim().replace(/\s+/g, " "),
+            InstitutionType: item.InstitutionType.trim()
+              .replace(/\s+/g, " ")
+              .toLowerCase(),
+          };
+          return trimmedItem;
+        }),
+      });
+    }
+    // await prisma.data.createMany({
+    //   data: results.map((item) => {
+    //     const trimmedItem = {
+    //       ...item,
+    //       INSTITUTION: item.INSTITUTION.trim().replace(/\s+/g, " "),
+    //       InstitutionType: item.InstitutionType.trim()
+    //         .replace(/\s+/g, " ")
+    //         .toLowerCase(),
+    //     };
+    //     return trimmedItem;
+    //   }),
+    // });
 
     console.log("Data created successfully.");
   } catch (e) {
