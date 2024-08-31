569304082608564082d255c78f3adc98eb9105e0
alidauda <alidauda14@gmail.com>
2024-08-31 15:13:10 +0100

    Refactor GET function in route.ts

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,5 +1,5 @@
 import prisma from "@/utlis/db";
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 const geopoliticalZones = [
   {
     zone: "North Central",
@@ -42,124 +42,125 @@ interface StateSums {
   [key: string]: number;
 }
 export async function GET(req: NextRequest) {
-  const searchParams = req.nextUrl.searchParams;
-  const query = searchParams.get("query");
-  const regionTotals: RegionTotals = {};
-  const totalsByYear: { [key: string]: number } = {};
-  const totoalsByYearForForeignStudent: { [key: string]: number } = {};
-  const totalsByInstutionType: { [key: string]: number } = {};
+  return NextResponse.json({ message: "Hello World" });
+  // const searchParams = req.nextUrl.searchParams;
+  // const query = searchParams.get("query");
+  // const regionTotals: RegionTotals = {};
+  // const totalsByYear: { [key: string]: number } = {};
+  // const totoalsByYearForForeignStudent: { [key: string]: number } = {};
+  // const totalsByInstutionType: { [key: string]: number } = {};
 
-  let data = await prisma.data.findMany({
-    orderBy: {
-      AdmissionYear: "asc",
-    },
-  });
-  if (query && query.trim() !== "") {
-    data = data.filter((item) => item.AdmissionYear === query.trim());
-  }
+  // let data = await prisma.data.findMany({
+  //   orderBy: {
+  //     AdmissionYear: "asc",
+  //   },
+  // });
+  // if (query && query.trim() !== "") {
+  //   data = data.filter((item) => item.AdmissionYear === query.trim());
+  // }
 
-  const stateValues = data.map(
-    ({
-      CourseTotal,
-      INSTITUTION,
-      InstitutionType,
-      AdmissionYear,
-      id,
-      Foreign,
-      COURSE,
-      ...rest
-    }) => rest
-  );
-  const stateSums = computeStateSums(stateValues);
-  const forAllinstu = ["university", "college", "polytechnic"].map((item) => ({
-    type: item,
-    data: computeStateSums(
-      data
-        .filter((insuType) => insuType.InstitutionType === item)
-        .map(
-          ({
-            CourseTotal,
-            INSTITUTION,
-            InstitutionType,
-            AdmissionYear,
-            id,
-            Foreign,
-            COURSE,
-            ...rest
-          }) => rest
-        )
-    ),
-  }));
-  let totalAdmission = getTotal(
-    data.map((item) => {
-      return item.CourseTotal;
-    })
-  );
-  let totalUniversityAdmission = getTotal(
-    data
-      .filter((item) => item.InstitutionType === "university")
-      .map((item) => item.CourseTotal)
-  );
-  let totalCollegeAdmission = getTotal(
-    data
-      .filter((item) => item.InstitutionType === "college")
-      .map((item) => item.CourseTotal)
-  );
-  let totalPolytenicAdmission = getTotal(
-    data
-      .filter((item) => item.InstitutionType === "polytechnic")
-      .map((item) => item.CourseTotal)
-  );
+  // const stateValues = data.map(
+  //   ({
+  //     CourseTotal,
+  //     INSTITUTION,
+  //     InstitutionType,
+  //     AdmissionYear,
+  //     id,
+  //     Foreign,
+  //     COURSE,
+  //     ...rest
+  //   }) => rest
+  // );
+  // const stateSums = computeStateSums(stateValues);
+  // const forAllinstu = ["university", "college", "polytechnic"].map((item) => ({
+  //   type: item,
+  //   data: computeStateSums(
+  //     data
+  //       .filter((insuType) => insuType.InstitutionType === item)
+  //       .map(
+  //         ({
+  //           CourseTotal,
+  //           INSTITUTION,
+  //           InstitutionType,
+  //           AdmissionYear,
+  //           id,
+  //           Foreign,
+  //           COURSE,
+  //           ...rest
+  //         }) => rest
+  //       )
+  //   ),
+  // }));
+  // let totalAdmission = getTotal(
+  //   data.map((item) => {
+  //     return item.CourseTotal;
+  //   })
+  // );
+  // let totalUniversityAdmission = getTotal(
+  //   data
+  //     .filter((item) => item.InstitutionType === "university")
+  //     .map((item) => item.CourseTotal)
+  // );
+  // let totalCollegeAdmission = getTotal(
+  //   data
+  //     .filter((item) => item.InstitutionType === "college")
+  //     .map((item) => item.CourseTotal)
+  // );
+  // let totalPolytenicAdmission = getTotal(
+  //   data
+  //     .filter((item) => item.InstitutionType === "polytechnic")
+  //     .map((item) => item.CourseTotal)
+  // );
 
-  data.forEach((item) => {
-    if (totalsByYear[item.AdmissionYear]) {
-      totalsByYear[item.AdmissionYear] += parseInt(item.CourseTotal);
-    } else {
-      totalsByYear[item.AdmissionYear] = parseInt(item.CourseTotal);
-    }
-  });
-  data.forEach((item) => {
-    if (totalsByInstutionType[item.InstitutionType]) {
-      totalsByInstutionType[item.InstitutionType] += parseInt(item.CourseTotal);
-    } else {
-      totalsByInstutionType[item.InstitutionType] = parseInt(item.CourseTotal);
-    }
-  });
-  data.forEach((item) => {
-    if (totoalsByYearForForeignStudent[item.AdmissionYear]) {
-      totoalsByYearForForeignStudent[item.AdmissionYear] += parseInt(
-        item.Foreign
-      );
-    } else {
-      totoalsByYearForForeignStudent[item.AdmissionYear] = parseInt(
-        item.Foreign
-      );
-    }
-  });
-  for (let i = 0; i < stateValues.length; i++) {
-    for (const [state, population] of Object.entries(stateValues[i]!)) {
-      for (const zone of geopoliticalZones) {
-        if (zone.states.includes(state)) {
-          regionTotals[zone.zone] =
-            (regionTotals[zone.zone] || 0) + parseInt(population);
-        }
-      }
-    }
-  }
-  return Response.json({
-    totalAdmission,
-    regionTotals,
-    stateSums,
-    institutionData: [
-      { institutionType: "university", data: totalUniversityAdmission },
-      { institutionType: "college", data: totalCollegeAdmission },
-      { institutionType: "poly", data: totalPolytenicAdmission },
-    ],
-    totalsByYear,
-    totoalsByYearForForeignStudent,
-    forAllinstu,
-    totalsByInstutionType,
-  });
+  // data.forEach((item) => {
+  //   if (totalsByYear[item.AdmissionYear]) {
+  //     totalsByYear[item.AdmissionYear] += parseInt(item.CourseTotal);
+  //   } else {
+  //     totalsByYear[item.AdmissionYear] = parseInt(item.CourseTotal);
+  //   }
+  // });
+  // data.forEach((item) => {
+  //   if (totalsByInstutionType[item.InstitutionType]) {
+  //     totalsByInstutionType[item.InstitutionType] += parseInt(item.CourseTotal);
+  //   } else {
+  //     totalsByInstutionType[item.InstitutionType] = parseInt(item.CourseTotal);
+  //   }
+  // });
+  // data.forEach((item) => {
+  //   if (totoalsByYearForForeignStudent[item.AdmissionYear]) {
+  //     totoalsByYearForForeignStudent[item.AdmissionYear] += parseInt(
+  //       item.Foreign
+  //     );
+  //   } else {
+  //     totoalsByYearForForeignStudent[item.AdmissionYear] = parseInt(
+  //       item.Foreign
+  //     );
+  //   }
+  // });
+  // for (let i = 0; i < stateValues.length; i++) {
+  //   for (const [state, population] of Object.entries(stateValues[i]!)) {
+  //     for (const zone of geopoliticalZones) {
+  //       if (zone.states.includes(state)) {
+  //         regionTotals[zone.zone] =
+  //           (regionTotals[zone.zone] || 0) + parseInt(population);
+  //       }
+  //     }
+  //   }
+  // }
+  // return Response.json({
+  //   totalAdmission,
+  //   regionTotals,
+  //   stateSums,
+  //   institutionData: [
+  //     { institutionType: "university", data: totalUniversityAdmission },
+  //     { institutionType: "college", data: totalCollegeAdmission },
+  //     { institutionType: "poly", data: totalPolytenicAdmission },
+  //   ],
+  //   totalsByYear,
+  //   totoalsByYearForForeignStudent,
+  //   forAllinstu,
+  //   totalsByInstutionType,
+  // });
 }
 
 function computeStateSums(stateValues: StateValues[]): StateSums {
